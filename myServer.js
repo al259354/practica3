6200b022cc853ef6c9b35d88f508eb45537d5666
al259354 <39095485+al259354@users.noreply.github.com>
2018-05-15 22:16:28 +0200

    Update myServer.js

--- myServer.js
+++ myServer.js
@@ -37,10 +37,10 @@ app.get('/stream/:name/geo',ctrl.geolocalizacion);
 
 
 ctrl.warmup.once("warmup", _ => {
-   console.log("Web server running on port 8080");
-   app.listen(8080);
+   console.log("Web server running on port 3030");
+   app.listen(3030);
 });
 
-app.listen(8080,	function	()	{
+app.listen(3030,	function	()	{
 		console.log("the	server	is	running!");
 });
